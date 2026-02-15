5571765b5706ca4e1643aaf0df87252ce51222fb
fredcallaway <fredc@llaway.com>
2026-02-15 11:02:28 -0500

    remove prolific.config

--- plugins/00.config.ts
+++ plugins/00.config.ts
@@ -1,6 +1,5 @@
 // Initialize configs before any components load
 import '~/epoch.config'
-import '~/prolific.config'
 
 export default defineNuxtPlugin(() => {
   // Configs are now initialized and available
