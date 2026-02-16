18ca2a4a7d3e28f8294064f0937a2c2c41c6576d
fredcallaway <fredc@llaway.com>
2026-02-15 21:24:22 -0500

    EDemoPhases

---
+++ components/EDemoPhases.vue
@@ -0,0 +1,51 @@
+<script lang="ts" setup>
+
+const epoch = usePhaseEpoch('demoPhases', ['apple', 'banana', 'choice', 'durian', 'date'])
+const { Phase, goToPhase } = useDisplayPhases(epoch.phases, { duration: 500 })
+watch(epoch.phase, (newPhase, oldPhase) => {
+  goToPhase(newPhase)
+})
+
+</script>
+
+<template>
+  <div w800px h300px>
+    <!-- constant means always mounted; just invisible when inactive -->
+    <!-- see also: persist which keeps it mounted between first and last active phase -->
+    <!-- static means it stays in the layout flow when not active (by default, it takes no space when inactive) -->
+    <Phase which="apple banana" constant static >
+      the phase is either apple or banana
+    </Phase>
+
+    <Phase which="apple" flex-center flex-col gap-5>
+      <div font-bold>apple</div>
+      <OnMounted :fn="() => console.log('apple mounted')"/>
+      <ESequence name="applesequence" text-lg>
+        <EContinue button>Red Delicious</EContinue>
+        <EContinue button>Granny Smith</EContinue>
+        <EContinue button>Honeycrisp</EContinue>
+      </ESequence>
+    </Phase>
+
+    <Phase which="banana" flex-center flex-col gap-5>
+      <div font-bold>banana</div>
+      <OnMounted :fn="() => console.log('banana mounted')"/>
+      <PButton value="next" @click="epoch.next" />
+    </Phase>
+
+    <Phase which="choice" flex-center flex-col gap-5>
+      <div font-bold>choice</div>
+      <PButtons values="durian date" @click="(value) => epoch.goTo(value as any)" />
+    </Phase>
+
+    <Phase which="durian" flex-center flex-col gap-5>
+      you chose durian
+      <PButton value="reset" @click="epoch.goTo(0)" />
+    </Phase>
+
+    <Phase which="date" flex-center flex-col gap-5>
+      you chose date
+      <PButton value="reset" @click="epoch.goTo(0)" />
+    </Phase>
+  </div>
+</template>
